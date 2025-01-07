6ca914b3fd7ef32f11bc797911f30256eb2d97c8
owdax <omid.dolatkia@gmail.com>
2025-01-07 22:47:22 +0330

    Updated noted app

--- Noted/NotedApp.swift
+++ Noted/NotedApp.swift
@@ -5,28 +5,28 @@
 //  Created by Omid DolatKia on 2024/10/31.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct NotedApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+	var sharedModelContainer: ModelContainer = {
+		let schema = Schema([
+			Note.self
+		])
+		let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+		do {
+			return try ModelContainer(for: schema, configurations: [modelConfiguration])
+		} catch {
+			fatalError("Could not create ModelContainer: \(error)")
+		}
+	}()
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
-    }
+	var body: some Scene {
+		WindowGroup {
+			HomeView()
+		}
+		.modelContainer(sharedModelContainer)
+	}
 }
